de8eaf58d532caf6d46937ab107b5da3e06a6a73
Petersson Dutta <peterssondutta@gmail.com>
2024-04-27 20:16:42 +0530

    initial

---
+++ Combined Project/pages/ChatBot.py
@@ -0,0 +1,44 @@
+
+import streamlit as st
+import google.generativeai as genai
+from dotenv import load_dotenv
+from IPython.display import display
+from IPython.display import Markdown
+from youtube_transcript_api import YouTubeTranscriptApi
+import os
+import PIL
+from PIL import Image
+
+
+
+load_dotenv()
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+
+model = genai.GenerativeModel('gemini-1.5-pro-latest')
+
+def get_gemini_conversation(prompt, uploaded_file):
+    if uploaded_file is not None:
+        response = model.generate_content([prompt, uploaded_file])
+    else:
+        response = model.generate_content(prompt)
+    return response
+
+
+def main():
+    st.title("Chatbot with Gemini")
+    user_prompt = st.text_area("You:", height=200)
+    uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png"])
+    img=None
+    if uploaded_file is not None:
+        img = PIL.Image.open(uploaded_file)
+    if st.button('Submit'):
+        if user_prompt:
+            response = get_gemini_conversation(user_prompt, img)
+            st.write(response.text)
+        else:
+            st.write("No prompt found!")
+
+        
+if __name__ == "__main__":
+    main()
---
+++ Combined Project/pages/Youtube Video Explainer.py
@@ -0,0 +1,61 @@
+# streamlit run app.py --server.maxUploadSize 400
+import streamlit as st
+import pathlib
+import textwrap
+import google.generativeai as genai
+from dotenv import load_dotenv
+from IPython.display import display
+from IPython.display import Markdown
+from youtube_transcript_api import YouTubeTranscriptApi
+
+def get_transcript(url):
+    video_id = url.split("=")[1]
+
+    # Retrieve the transcript using YouTubeTranscriptApi
+    transcript_list = YouTubeTranscriptApi.get_transcript(video_id)
+
+    # Convert the list of dictionaries to a single string
+    transcript_string = " ".join([item['text'] for item in transcript_list])
+    return transcript_string
+
+
+# def to_markdown(text):
+#   text = text.replace('•', '  *')
+#   return Markdown(textwrap.indent(text, '> ', predicate=lambda _: True))
+
+st.title("Youtube Video Explainer")
+# st.markdown("**Enter the the Video ID**", unsafe_allow_html=True)
+st.header("Enter the the Youtube Video URL:")
+url = st.text_input("")
+
+prompt=""
+# st.header("Enter the prompt if you want:")
+st.markdown("**Default prompt:** Make a note of whatever taught in this transcript elaborately and explain each of the concepts and terms discussed. Explain in a detailed manner. Expound and interpret the terms.", unsafe_allow_html=True)
+# # video_id = st.text_area("Enter Below", height=200)
+# prompt= st.text_area("Enter Below", height=200)
+
+import os
+load_dotenv()
+GOOGLE_API_KEY=os.getenv('GOOGLE_API_KEY')
+genai.configure(api_key=GOOGLE_API_KEY)
+
+# for m in genai.list_models():
+#   if 'generateContent' in m.supported_generation_methods:
+#     print(m.name)
+transcript_string=""
+if url:
+    transcript_string = get_transcript(url)
+
+if transcript_string:
+    try: 
+        model = genai.GenerativeModel('gemini-1.5-pro-latest')
+        if prompt:
+            response = model.generate_content(f"{prompt} Transcript: {transcript_string}")
+        else:
+            response = model.generate_content(f"Make a note of whatever taught in this transcript elaborately and explain each of the concepts and terms discussed. Explain in a detailed manner. Expound and interpret the terms. Transcript: {transcript_string}")
+        st.write(response.text)
+        
+    except Exception as e:
+         st.write(f'{type(e).__name__}: {e}')
+
+
